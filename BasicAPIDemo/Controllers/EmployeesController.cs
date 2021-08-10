8b291a385993a0f471cfe37922c9dbfc159be521
Allan <allanboiebiaspunzalan@gmail.com>
2021-08-10 15:10:34 +0800

    Initial Commits

---
+++ BasicAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BasicAPIDemo.Data;
+using BasicAPIDemo.Models;
+
+namespace BasicAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly BasicAPIDemoContext _context;
+
+        public EmployeesController(BasicAPIDemoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+        {
+            List<Employee> employee = new List<Employee>();
+            employee = _context.Employee.ToList();
+            //LINQ to EF
+            Employee employee1 = new Employee();
+            List<Employee> listemployee = new List<Employee>();
+            employee1 = employee.Where(x => x.EmployeeID == "1").FirstOrDefault();
+            //listemployee = listemployee.Where(x => x.Firstname == "Nagbago").LastOrDefault();
+            int empCount = employee.Count();
+            return _context.Employee;
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(string id)
+        {
+            var employee = await _context.Employee.FindAsync(id);
+          
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(string id, Employee employee)
+        {
+            if (id != employee.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _context.Employee.Add(employee);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EmployeeExists(employee.EmployeeID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetEmployee", new { id = employee.EmployeeID }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(string id)
+        {
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employee.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return employee;
+        }
+
+        private bool EmployeeExists(string id)
+        {
+            return _context.Employee.Any(e => e.EmployeeID == id);
+        }
+    }
+}
---
+++ BasicAPIDemo/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAPIDemo.Models
+{
+    public class Employee
+    {
+            public string EmployeeID { get; set; }
+
+            public string Firstname { get; set; }
+
+            public string Lastname { get; set; }
+
+            public int Age { get; set; }
+
+            public bool isActive { get; set; }
+
+    }
+}
